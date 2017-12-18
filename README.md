c00faa385a4acfab552314e0c0bf283657ea5c20
laurendimitra <lauren.mooore@bluetruckstudios.co>
2017-12-17 20:29:28 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bamazonrepo
+
+Video: 
+<div style="position:relative;height:0;padding-bottom:75.0%"><iframe src="https://www.youtube.com/embed/QhjDO7C2olQ?ecver=2" width="480" height="360" frameborder="0" gesture="media" allow="encrypted-media" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,63 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  // Your username
   user: "root",
   database: "bamazon_DB"
 });
-connection.connect(function(err) {
+
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   afterConnection();
 });
+
 function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     console.log(res);
-    connection.end();
+    purchase();
   });
+}
+
+function purchase() {
+  inquirer.prompt([{
+      type: 'input',
+      name: 'item_id',
+      message: 'What is the ID number of the product you would like to purchase?',
+      filter: Number
+    },
+    {
+      type: 'input',
+      name: 'amount',
+      message: 'How much of this product would you like to purchase?',
+      filter: Number
+    }
+  ]).then(function (answer) {
+    var itemID = answer.item_id;
+    var amount = answer.amount;
+    var queryStr = 'SELECT * FROM products WHERE ?';
+
+    connection.query(queryStr, {
+      item_id: itemID
+    }, function (err, data) {
+      if (err) throw err;
+
+      else {
+        var productData = data[0];
+        if (amount <= productData.stock_quantity) {
+          console.log('Great!');
+          var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - amount) + ' WHERE item_id = ' + itemID;
+          connection.query(updateQueryStr, function (err, data) {
+            if (err) throw err;
+            console.log('Your oder has been placed. Your total is $' + productData.price * amount);
+            connection.end();
+          })
+        } else {
+          console.log('Sorry, there is not enough in stock to complete your order. Please try again.');
+        }
+      }
+    })
+  })
 }
